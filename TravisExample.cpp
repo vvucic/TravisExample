9ed96a3fa7d511d101d5ff6373f1c1ac63b23bbf
Vucic Vladimir <vladimir.vucic@afconsult.com>
2016-08-31 15:28:20 +0200

    First commit

---
+++ TravisExample.cpp
@@ -0,0 +1,17 @@
+// TravisExample.cpp : Defines the entry point for the console application.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	int x, y, z;
+	std::cout << "Unesite x i y" << std::endl;
+	std::cin >> x >> y;
+	z = x + y;
+	std::cout << "Zbir je : " << z << std::endl;
+    return 0;
+}
+
